6dfc1ab81de21eb57dd0b812698fcb1c5184bcd0
OCC Secretary <secretary@oceansidehall.com>
2024-06-11 14:34:34 -0700

    Update theme colors and main nav

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,6 +1,6 @@
 export const url = process.env.URL || 'http://localhost:8080';
-export const siteName = 'Eleventy Excellent';
-export const siteDescription = 'Eleventy starter for building modern, resilient websites';
+export const siteName = 'Oceanside Community Club';
+export const siteDescription = 'Community Club Information';
 export const siteType = 'Person'; // schema
 export const locale = 'en_EN';
 export const lang = 'en';
@@ -17,7 +17,7 @@ export const creator = {
   website: 'https://oceansidehall.com'
 };
 export const pathToSvgLogo = 'src/assets/svg/misc/logo.svg'; // used for favicon generation
-export const themeColor = '#DD4462'; //  Manifest: defines the default theme color for the application
+export const themeColor = '#336699'; //  Manifest: defines the default theme color for the application
 export const themeBgColor = '#FBFBFB'; // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
 export const opengraph_default = '/assets/images/template/opengraph-default.jpg'; // fallback/default meta image
 export const opengraph_default_alt = "The Oceanside Community Club"; // alt text for default meta image"
